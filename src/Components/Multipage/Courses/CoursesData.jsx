145aad159f0674fbb53031f2a287785d6b058b27
amodmanjarekar <amodm.1102@gmail.com>
2023-07-01 18:00:33 +0530

    func - Create ButtonData functionality

--- src/Components/Multipage/Courses/CoursesData.jsx
+++ src/Components/Multipage/Courses/CoursesData.jsx
@@ -2,227 +2,281 @@ export const fashion = {
   diploma: {
     diploma_basic: {
       name: "Diploma",
-      fy: {
-        name: "Year One",
-        sem1: {
-          name: "Semester One",
-          subjects: [
-            "Elements of Design",
-            "Fashion Industrial Language",
-            "Basic principle of Fashion",
-            "Stitching methods and application",
-            "Draping – 1",
-            "Practical",
-          ],
-          credits: 20,
+      years: [
+        {
+          name: "Year One",
+          sem1: {
+            name: "Semester One",
+            subjects: [
+              "Elements of Design",
+              "Fashion Industrial Language",
+              "Basic principle of Fashion",
+              "Stitching methods and application",
+              "Draping – 1",
+              "Practical",
+            ],
+            credits: 20,
+          },
+          sem2: {
+            name: "Semester Two",
+            subjects: [
+              "Fashion Illustration",
+              "Different body drape elements a function of clothing",
+              "Introduction to textile theory",
+              "History of costumes",
+              "Introduction to pattern making",
+              "Practical",
+            ],
+            credits: 20,
+          },
         },
-        sem2: {
-          name: "Semester Two",
-          subjects: [
-            "Fashion Illustration",
-            "Different body drape elements a function of clothing",
-            "Introduction to textile theory",
-            "History of costumes",
-            "Introduction to pattern making",
-            "Practical",
-          ],
-          credits: 20,
-        },
-      },
+      ],
     },
     diploma_adv: {
       name: "Advanced Diploma",
-      fy: {
-        name: "Year One",
-        sem1: {
-          name: "Semester One",
-          subjects: [
-            "Foundation Art",
-            "Elements of principle of fashion design",
-            "Introduction of Pattern making & GMT",
-            "Practical – 1 Portfolio Making",
-            "Practical – 2 Pattern Making& Grading",
-          ],
-          credits: 20,
+      years: [
+        {
+          name: "Year One",
+          sem1: {
+            name: "Semester One",
+            subjects: [
+              "Foundation Art",
+              "Elements of principle of fashion design",
+              "Introduction of Pattern making & GMT",
+              "Practical – 1 Portfolio Making",
+              "Practical – 2 Pattern Making& Grading",
+            ],
+            credits: 20,
+          },
+          sem2: {
+            name: "Semester Two",
+            subjects: [
+              "History of the western world fashion",
+              "Introduction to textile",
+              "Grooming & Communication Skills",
+              "Fashion a design foundation",
+              "Creative yarn craft",
+              "Basics of Computer Application Practical",
+            ],
+            credits: 20,
+          },
         },
-        sem2: {
-          name: "Semester Two",
-          subjects: [
-            "History of the western world fashion",
-            "Introduction to textile",
-            "Grooming & Communication Skills",
-            "Fashion a design foundation",
-            "Creative yarn craft",
-            "Basics of Computer Application Practical",
-          ],
-          credits: 20,
+        {
+          name: "Year Two",
+          sem1: {
+            name: "Semester Three",
+            subjects: [
+              "Introduction to fashion illustration",
+              "Sewing Technology",
+              "Introduction to draping",
+              "Creative Embroidery",
+              "Practical – 1 Inspirations on Project",
+              "Practical – 2 Pattern Making & Sewing",
+            ],
+            credits: 20,
+          },
+          sem2: {
+            name: "Semester Four",
+            subjects: [
+              "Garment Construction",
+              "Sewing Technology part – II",
+              "Textile science processing",
+              "Advance Computer",
+              "Practical – 1 Portfolio Development",
+            ],
+            credits: 20,
+          },
         },
-      },
-      sy: {
-        name: "Year Two",
-        sem3: {
-          name: "Semester Three",
-          subjects: [
-            "Introduction to fashion illustration",
-            "Sewing Technology",
-            "Introduction to draping",
-            "Creative Embroidery",
-            "Practical – 1 Inspirations on Project",
-            "Practical – 2 Pattern Making & Sewing",
-          ],
-          credits: 20,
-        },
-        sem4: {
-          name: "Semester Four",
-          subjects: [
-            "Garment Construction",
-            "Sewing Technology part – II",
-            "Textile science processing",
-            "Advance Computer",
-            "Practical – 1 Portfolio Development",
-          ],
-          credits: 20,
-        },
-      },
+      ],
     },
     diploma_ug: {
       name: "Diploma Undergraduate",
-      fy: {
-        name: "Year One",
-        sem1: {
-          name: "Semester One",
-          subjects: [
-            "Foundation Art",
-            "Introduction to pattern making",
-            "Fashion Illustration",
-            "History of costumes",
-            "Practical-1 sewing technology",
-            "Practical-2 fashion project",
-          ],
-          credits: 20,
+      years: [
+        {
+          name: "Year One",
+          sem1: {
+            name: "Semester One",
+            subjects: [
+              "Foundation Art",
+              "Introduction to pattern making",
+              "Fashion Illustration",
+              "History of costumes",
+              "Practical-1 sewing technology",
+              "Practical-2 fashion project",
+            ],
+            credits: 20,
+          },
+          sem2: {
+            name: "Semester Two",
+            subjects: [
+              "Design Foundation",
+              "Introduction to textile",
+              "Introduction to PM part 2",
+              "Basic embroidery",
+              "Practical-1 CAD",
+              "Practical-2 GMT",
+            ],
+            credits: 20,
+          },
         },
-        sem2: {
-          name: "Semester Two",
-          subjects: [
-            "Design Foundation",
-            "Introduction to textile",
-            "Introduction to PM part 2",
-            "Basic embroidery",
-            "Practical-1 CAD",
-            "Practical-2 GMT",
-          ],
-          credits: 20,
+        {
+          name: "Year Two",
+          sem1: {
+            name: "Semester Three",
+            subjects: [
+              "Adv. Fashion illustration",
+              "Introduction to draping",
+              "Pattern making techniques part 1",
+              "History of Indian costumes",
+              "Practical-1 inspiration on project",
+              "Practical-2 Garment project",
+            ],
+            credits: 20,
+          },
+          sem2: {
+            name: "Semester Four",
+            subjects: [
+              "Apparel design",
+              "Theory of dyeing",
+              "PMT AND GMT",
+              "Traditional embroidery",
+              "Practical-1 CAD",
+              "Practical-2 dyeing project",
+            ],
+            credits: 20,
+          },
         },
-      },
-      sy: {
-        name: "Year Two",
-        sem3: {
-          name: "Semester Three",
-          subjects: [
-            "Adv. Fashion illustration",
-            "Introduction to draping",
-            "Pattern making techniques part 1",
-            "History of Indian costumes",
-            "Practical-1 inspiration on project",
-            "Practical-2 Garment project",
-          ],
-          credits: 20,
+        {
+          name: "Year Three",
+          sem1: {
+            name: "Semester Five",
+            subjects: [
+              "Fashion Analysis",
+              "Quality control",
+              "Int. TO Printing techniques",
+              "History of western world fasion",
+              "Practical-1 fasion forecasting",
+              "Practical-2 men wear project",
+            ],
+            credits: 20,
+          },
+          sem2: {
+            name: "Semester Six",
+            subjects: [
+              "Advance Fashion Illustration - II",
+              "Surface Ornamentation",
+              "Fashion Merchandising",
+              "Industry project & Viva",
+              "Fashion Presentation",
+              "Portfolio Development",
+            ],
+            credits: 20,
+          },
         },
-        sem4: {
-          name: "Semester Four",
-          subjects: [
-            "Apparel design",
-            "Theory of dyeing",
-            "PMT AND GMT",
-            "Traditional embroidery",
-            "Practical-1 CAD",
-            "Practical-2 dyeing project",
-          ],
-          credits: 20,
-        },
-      },
-      ty: {
-        name: "Year Three",
-        sem5: {
-          name: "Semester Five",
-          subjects: [
-            "Fashion Analysis",
-            "Quality control",
-            "Int. TO Printing techniques",
-            "History of western world fasion",
-            "Practical-1 fasion forecasting",
-            "Practical-2 men wear project",
-          ],
-          credits: 20,
-        },
-        sem6: {
-          name: "Semester Six",
-          subjects: [
-            "Advance Fashion Illustration - II",
-            "Surface Ornamentation",
-            "Fashion Merchandising",
-            "Industry project & Viva",
-            "Fashion Presentation",
-            "Portfolio Development",
-          ],
-          credits: 20,
-        },
-      },
+      ],
     },
     diploma_pg: {
       name: "Diploma Postgraduate",
-      fy: {
-        name: "Year One",
-        sem1: {
-          name: "Semester One",
-          subjects: [
-            "Basics of Designing",
-            "Pattern Making Techniques",
-            "Fashion Illustration",
-            "History of Costumes",
-            "Practical - Production Development",
-          ],
-          credits: 20,
+      years: [
+        {
+          name: "Year One",
+          sem1: {
+            name: "Semester One",
+            subjects: [
+              "Basics of Designing",
+              "Pattern Making Techniques",
+              "Fashion Illustration",
+              "History of Costumes",
+              "Practical - Production Development",
+            ],
+            credits: 20,
+          },
+          sem2: {
+            name: "Semester Two",
+            subjects: [
+              "Fashion Foundation",
+              "Textile Theory",
+              "Pattern Developing",
+              "Practical-1 CAD",
+              "Practical-2 GMT",
+            ],
+            credits: 20,
+          },
         },
-        sem2: {
-          name: "Semester Two",
-          subjects: [
-            "Fashion Foundation",
-            "Textile Theory",
-            "Pattern Developing",
-            "Practical-1 CAD",
-            "Practical-2 GMT",
-          ],
-          credits: 20,
+        {
+          name: "Year Two",
+          sem1: {
+            name: "Semester Three",
+            subjects: [
+              "Advance Fashion Illustration",
+              "Advance Draping",
+              "Surface Ornamentation",
+              "Practical-1 Industry Project & Viva",
+              "Practical-2 PMT & GMT",
+            ],
+            credits: 20,
+          },
+          sem2: {
+            name: "Semester Four",
+            subjects: [
+              "Quality Control Technology",
+              "Dyeing & Printing",
+              "Fashion Merchandising - A retailing",
+              "Traditional textiles and State Embroidery",
+              "CAD",
+              "GMT & PMT Project",
+              "Portfolio Development",
+            ],
+            credits: 20,
+          },
         },
-      },
-      sy: {
-        name: "Year Two",
-        sem3: {
-          name: "Semester Three",
-          subjects: [
-            "Advance Fashion Illustration",
-            "Advance Draping",
-            "Surface Ornamentation",
-            "Practical-1 Industry Project & Viva",
-            "Practical-2 PMT & GMT",
-          ],
-          credits: 20,
-        },
-        sem4: {
-          name: "Semester Four",
-          subjects: [
-            "Quality Control Technology",
-            "Dyeing & Printing",
-            "Fashion Merchandising - A retailing",
-            "Traditional textiles and State Embroidery",
-            "CAD",
-            "GMT & PMT Project",
-            "Portfolio Development",
-          ],
-          credits: 20,
-        },
-      },
+      ],
     },
   },
+};
+
+export const interior = {
+  diploma: {
+    diploma_basic: {},
+    diploma_adv: {},
+    diploma_ug: {},
+    diploma_pg: {},
+  },
+
+  bachelors: {
+    bsc: {
+      name: "B.Sc in Interior Design & Technology",
+      years: [
+        {
+          name: "Year One",
+          sem1: {
+            name: "Semester One",
+            subjects: ["Basics of Drafting", "Basics of Graphics"],
+            credits: 20,
+          },
+          sem2: {
+            name: "Semester Two",
+            subjects: [],
+            credits: 20,
+          },
+        },
+        {
+          name: "Year Two",
+          sem1: {
+            name: "Semester Three",
+            subjects: [],
+            credits: 20,
+          },
+          sem2: {
+            name: "Semester Four",
+            subjects: [],
+            credits: 20,
+          },
+        },
+        { name: "Year Three", sem1: {}, sem2: {} },
+      ],
+    },
+  },
+  masters: {},
+  certificate: {},
 };
